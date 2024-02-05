b5349e2e1ba084190e85478d2da194177b30a7b6
MatTeo-BT <aboutmet@gmail.com>
2024-02-05 09:18:52 +0100

    esercizio completo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,23 +1,22 @@
 <!DOCTYPE html>
 <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>@yield('title','Laravel App Title')</title>
+        <link rel="shortcut icon" href="{{ Vite::asset('resources/img/favicon.ico') }}" type="image/x-icon">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+        @vite('resources/js/app.js')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>@yield('title','Laravel App Title')</title>
-    @vite('resources/js/app.js')
+        @yield('head-content')
+    </head>
+    <body>
+        @include('layouts.partials.header')
 
-    @yield('head-content')
-</head>
+        <main>
+            @yield('main-content')
+        </main>
 
-<body>
-    @include('layouts.partials.header')
-
-    <main>
-        @yield('main-content')
-    </main>
-
-    @include('layouts.partials.footer')
-</body>
-
+        @include('layouts.partials.footer')
+    </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('pages.home');
+});
+
+Route::get('/comics', function () {
+    $comics = config('db.comics');
+    return view('pages.comics', ['comics' => $comics]);
 });
